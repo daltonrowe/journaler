9301a9f4b06e0792817d616b5e689169e7dac8ad
Dalton <cdaltonrowe@gmail.com>
2026-01-11 12:58:24 +0100

    static assets too

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: "5mb" }));
-app.use(express.static(path.join(process.cwd(), "assets")));
+app.use(express.static(path.join(root, "assets")));
 app.use(express.static(config.dir));
 
 app.get("/", sendView);
